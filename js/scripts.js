c31710c46d0f99269bf52b09f461b0c0c2098ab3
amberelle <amber.baird@gmail.com>
2016-03-25 12:24:02 -0700

    add name field and more pictures

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 
 $(document).ready(function() {
-  $("#submit").click(function(event) {
 
+
+  $("#submit").click(function(event) {
+  // $("#blanks").submit(function(event){}
+
+
+    var personInput = $("input#person").val();
     var purpose = $("#purpose").val();
     var locale = $("#locale").val();
+
+    $(".person").text(personInput);
 
 
       event.preventDefault();
@@ -14,70 +21,192 @@ $(document).ready(function() {
         $("#findMe").text("Paris, France");
             $("#paris").show();
             $("#peru").hide();
+            $("#rocky").hide();
+            $("#koh").hide();
+            $("#hood").hide();
+            $("#rome").hide();
+            $("#hawaii").hide();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").hide();
+
+
       }
       if (locale === "tropical") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
+        $("#findMe").text("Koh Ta Kiev, Cambodia");
+            $("#paris").hide();
+            $("#peru").hide();
+            $("#rocky").hide();
+            $("#koh").show();
+            $("#hood").hide();
+            $("#rome").hide();
+            $("#hawaii").hide();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").hide();
       }
       if (locale === "mountains") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
+        $("#findMe").text("Mt. Hood, Oregon");
+            $("#paris").hide();
+            $("#peru").hide();
+            $("#rocky").hide();
+            $("#koh").hide();
+            $("#hood").show();
+            $("#rome").hide();
+            $("#hawaii").hide();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").hide();
       }
     }
     if (purpose  === "sites") {
       // $("#george").toggle();
       if (locale === "urban") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
+        $("#findMe").text("Rome, Italy");
+            $("#paris").hide();
+            $("#peru").hide();
+            $("#rocky").hide();
+            $("#koh").hide();
+            $("#hood").hide();
+            $("#rome").show();
+            $("#hawaii").hide();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").hide();
       }
       if (locale === "tropical") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
+            $("#findMe").text("Hawaii");
+            $("#paris").hide();
+            $("#peru").hide();
+            $("#rocky").hide();
+            $("#koh").hide();
+            $("#hood").hide();
+            $("#rome").hide();
+            $("#hawaii").show();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").hide();
       }
       if (locale === "mountains") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
+        $("#findMe").text("Rougon, France");
+            $("#paris").hide();
+            $("#peru").hide();
+            $("#rocky").hide();
+            $("#koh").hide();
+            $("#hood").hide();
+            $("#rome").hide();
+            $("#hawaii").hide();
+            $("#rougon").show();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").hide();
       }
     }
     if (purpose  === "adventure") {
       // $("#george").toggle();
       if (locale === "urban") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
+        $("#findMe").text("Boulder, Colorado");
+          $("#paris").hide();
+          $("#peru").hide();
+          $("#rocky").hide();
+          $("#koh").hide();
+          $("#hood").hide();
+          $("#rome").hide();
+          $("#hawaii").hide();
+          $("#rougon").hide();
+          $("#denver").show();
+          $("#costa").hide();
+          $("#bang").hide();
+          $("#haiti").hide();
       }
       if (locale === "tropical") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
-      }
+        $("#findMe").text("Costa Rica");
+          $("#paris").hide();
+          $("#peru").hide();
+          $("#rocky").hide();
+          $("#koh").hide();
+          $("#hood").hide();
+          $("#rome").hide();
+          $("#hawaii").hide();
+          $("#rougon").hide();
+          $("#denver").hide();
+          $("#costa").show();
+          $("#bang").hide();
+          $("#haiti").hide();
+        }
       if (locale === "mountains") {
         $("#findMe").text("Mountains of Peru");
-            $("#peru").show();
             $("#paris").hide();
+            $("#peru").show();
+            $("#rocky").hide();
+            $("#koh").hide();
+            $("#hood").hide();
+            $("#rome").hide();
+            $("#hawaii").hide();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").hide();
       }
     }
     if (purpose  === "volunteer") {
       // $("#george").toggle();
       if (locale === "urban") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
-      }
+        $("#findMe").text("Bangkok, Thailand");
+            $("#paris").hide();
+            $("#peru").hide();
+            $("#rocky").hide();
+            $("#koh").hide();
+            $("#hood").hide();
+            $("#rome").hide();
+            $("#hawaii").hide();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").show();
+            $("#haiti").hide();
+          }
       if (locale === "tropical") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
-      }
+        $("#findMe").text("Haiti");
+            $("#paris").hide();
+            $("#peru").hide();
+            $("#rocky").hide();
+            $("#koh").hide();
+            $("#hood").hide();
+            $("#rome").hide();
+            $("#hawaii").hide();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").show();
+          }
       if (locale === "mountains") {
-        $("#findMe").text("Steve Buscemi");
-            $("#img1").show();
-            $("#img2").hide();
+        $("#findMe").text("Rocky Mountains");
+            $("#paris").hide();
+            $("#peru").hide();
+            $("#rocky").show();
+            $("#koh").hide();
+            $("#hood").hide();
+            $("#rome").hide();
+            $("#hawaii").hide();
+            $("#rougon").hide();
+            $("#denver").hide();
+            $("#costa").hide();
+            $("#bang").hide();
+            $("#haiti").hide();
       }
     }
 
